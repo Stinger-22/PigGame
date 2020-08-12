e6b538ae030b834aef14cad6543c34240f8c3075
Stinger-22 <56128124+Stinger-22@users.noreply.github.com>
2020-08-12 22:26:03 +0300

    Update WorldBuilder.py
    Clear variable names

--- WorldBuilder.py
+++ WorldBuilder.py
@@ -6,8 +6,8 @@ class WorldBuilder:
     def __init__(self):
         tile_size = 32
         x, y = 0, 0
-        self.tile_group = []
-        self.connected_tiles = []
+        self.tiles_group = []
+        self.mergedtiles_group = []
         # current_coords = [x, y]
         map1 = ['110',
                 '010',
@@ -20,26 +20,26 @@ class WorldBuilder:
                     x += tile_size
                 elif tile == '1':
                     add_tile = [x, y, x + tile_size, y + tile_size]
-                    self.tile_group.append(add_tile)
+                    self.tiles_group.append(add_tile)
                     x += tile_size
             y += tile_size
-        self.to_connect_tiles(self.tile_group)
+        self.merge_tiles(self.tiles_group)
 
     # Малюю
     # .........
 
     # Об'єдную
-    def to_connect_tiles(self, tiles):
+    def merge_tiles(self, tiles):
         if tiles == []:
             return
         else:
-            tile = self.tile_group[0]
+            tile = tiles[0]
             while len(tiles) > 1 and tile[2] == tiles[1][0] and tile[1] == tiles[1][1] and \
             tile[3] == tiles[1][3]:
                 tile[2], tile[3] = tiles[1][2], tiles[1][3]
                 tiles.pop(1)
-            self.connected_tiles.append(tile)
+            self.mergedtiles_group.append(tile)
             tiles.pop(0)
-            self.to_connect_tiles(tiles)
+            self.merge_tiles(tiles)
 
 world = WorldBuilder()
